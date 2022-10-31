1f6c9a19a79084d814cade8039d835cbdd698eba
SMHuTH8VsL <nbs56@georgetown.edu>
2022-10-31 12:42:46 -0400

    Added challenge A code in unemployment_solution_csv.py  file

--- unemployment_solution_csv.py
+++ unemployment_solution_csv.py
@@ -12,3 +12,26 @@ API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}&datatype=csv"
 
 df = read_csv(request_url)
+
+import os
+from pprint import pprint
+
+from dotenv import load_dotenv
+from pandas import read_csv
+from plotly.express import line
+
+load_dotenv()
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}&datatype=csv"
+
+df = read_csv(request_url)
+
+# Challenge A
+# What is the most recent unemployment rate? And the corresponding date? Display the unemployment rate using a percent sign.
+
+print("-------------------------")
+print("LATEST UNEMPLOYMENT RATE:")
+first_row = df.iloc[0]
+print(f"{first_row['value']}%", "as of", first_row["timestamp"])
