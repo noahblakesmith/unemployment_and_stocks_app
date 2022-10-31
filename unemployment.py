ad04d3a2c9c9494b9cec06f12e1bdf80dc2f1bc7
SMHuTH8VsL <nbs56@georgetown.edu>
2022-10-31 12:34:00 -0400

    Added challenge B code in unemployment.py file

--- unemployment.py
+++ unemployment.py
@@ -21,11 +21,23 @@ parsed_response = json.loads(response.text)
 data = parsed_response["data"]
 
 # Challenge A
-
-# What is the most recent unemployment rate? And the corresponding date?
-# Display the unemployment rate using a percent sign.
+# What is the most recent unemployment rate? And the corresponding date? Display the unemployment rate using a percent sign.
 
 print("-------------------------")
 print("LATEST UNEMPLOYMENT RATE:")
 #print(data[0])
 print(f"{data[0]['value']}%", "as of", data[0]["date"])
+
+# Challenge B
+# What is the average unemployment rate for all months during this calendar year? How many months does this cover?
+
+from statistics import mean
+
+this_year = [d for d in data if "2022-" in d["date"]]
+
+rates_this_year = [float(d["value"]) for d in this_year]
+#print(rates_this_year)
+
+print("-------------------------")
+print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
+print("NO MONTHS:", len(this_year))
