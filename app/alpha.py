aed4c42d58930962dcab0e9d3616d09d117b8dc6
SMHuTH8VsL <nbs56@georgetown.edu>
2022-11-11 20:11:52 -0500

    Added app folder

---
+++ app/alpha.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
--- app/unemployment_solution_json.py
+++ app/unemployment_solution_json.py
@@ -1,16 +1,10 @@
-
-# this is the "app/unemployment_report.py" file...
-
-import os
 import json
 from pprint import pprint
-
 import requests
-from dotenv import load_dotenv
 
-load_dotenv()
+from .alpha import API_KEY
 
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
 
 request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
 
