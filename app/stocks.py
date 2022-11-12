273745ff8c69593b4ff3a9be07ba4827cdd88e67
SMHuTH8VsL <nbs56@georgetown.edu>
2022-11-11 22:23:24 -0500

    Added data fetching test

--- app/stocks.py
+++ app/stocks.py
@@ -1,19 +1,26 @@
+from pandas import read_csv
+from app.alpha import API_KEY
+
 def format_usd(my_price):
     return f"${my_price:,.2f}"
 
-if __name__ == "__main__":
-
-    from pandas import read_csv
-    from app.alpha import API_KEY
-
-    print("STOCKS REPORT...")
-
-    symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
-    print("SYMBOL:", symbol)
-
+def fetch_stocks_data(symbol):
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&datatype=csv"
 
     df = read_csv(request_url)
+
+    return df
+
+if __name__ == "__main__":
+
+    print("STOCKS REPORT...")
+
+    symbol = input("Please input a stock symbol (default: 'NFLX'): ") or "NFLX"
+    
+    print("SYMBOL:", symbol)
+
+    df = fetch_stocks_data(symbol)
+
     print(df.columns)
     print(df.head())
 
--- test/stocks_test.py
+++ test/stocks_test.py
@@ -1,6 +1,18 @@
-from app.stocks import format_usd
+from app.stocks import format_usd, fetch_stocks_data
+from pandas import DataFrame
 
 def test_usd_formatting():
     assert format_usd(4.5) == "$4.50"
     assert format_usd(4.555555555555) == "$4.56"
     assert format_usd(1000000) == "$1,000,000.00"
+
+def test_data_fetching():
+    result = fetch_stocks_data("NFLX")
+    assert isinstance(result, DataFrame)
+    
+    assert "timestamp" in result.columns
+    assert "adjusted_close" in result.columns
+    assert "high" in result.columns
+    assert "low" in result.columns
+
+    assert len(result) >= 100
